9283b9be016ebad0878fa9f891192a91ddbf09aa
pavanvora <pavanvora.cs@gmail.com>
2019-11-21 09:29:34 +0530

    comment

--- exchange/addressList.js
+++ exchange/addressList.js
@@ -1,3 +1,7 @@
+/**
+ * Update the below array with addresses you want to listen for payment
+ */
+
 module.exports = [
   "GCVVI6C3NEID5USR54K646SOGTZ6AEGN7KT3ZK55XOE3T2W7JL33POMA",
   "GDIH675CDN2RHXMU4R7OQZZNGL3NBO3DEX3GKLFLCVQNZOZ5EHS3DZJZ"
